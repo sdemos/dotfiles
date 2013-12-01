e3efdcb46e412a3e9fc22f54032fdfea83025ee1
Stephen Demos <stphndemos@gmail.com>
2013-12-01 17:17:33 -0500

    fixed the default git protocol

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "Cloning dotfiles"
-hash git >/dev/null && /usr/bin/env git clone https://github.com/stphndemos/dotfiles.git ~/.genconfig || {
+hash git >/dev/null && /usr/bin/env git clone git@github.com:stphndemos/dotfiles.git ~/.genconfig || {
   echo "git not installed"
   exit
 }
