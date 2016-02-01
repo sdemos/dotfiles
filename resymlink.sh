bbada0c88d205bb163d87009fbe15c3c6927ac60
Stephen Demos <stphndemos@gmail.com>
2016-02-01 12:45:36 -0500

    gentoo uses .Xdefaults

--- resymlink.sh
+++ resymlink.sh
@@ -4,7 +4,6 @@ ln -sf ~/.genconfig/.vim_backup ~/.vim
 ln -sf ~/.genconfig/.xmonad_git ~/.xmonad
 ln -f ~/.genconfig/.*rc* ~/
 ln -f ~/.genconfig/.Xdefaults ~/
-ln -f ~/.genconfig/.Xresources ~/
 ln -f ~/.genconfig/.gitignore_global ~/
 hash git >/dev/null && /usr/bin/env git config --global core.excludesfile ~/.gitignore_global || {
   echo "git not installed (how did you get here?)"
