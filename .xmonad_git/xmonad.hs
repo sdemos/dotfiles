5346076c93c9ea6f600753e64bc622ad49a1a90f
Stephen Demos <stphndemos@gmail.com>
2016-02-02 13:00:34 -0500

    better xmonad configuration

--- .xmonad_git/xmonad.hs
+++ .xmonad_git/xmonad.hs
@@ -1,14 +1,20 @@
--- default desktop configuration for Fedora
-
 import System.Posix.Env (getEnv)
 import Data.Maybe (maybe)
+import Data.Bits ((.|.))
 
 import XMonad
 import XMonad.Hooks.SetWMName
+import XMonad.Util.CustomKeys
 
 main = xmonad $ defaultConfig
     { borderWidth       = 0
-    , terminal          = "uxterm" 
+    , terminal          = "urxvtc" 
+    -- this is to fix java awt failing because it has a whitelist of wms
     , startupHook       = setWMName "LG3D"
     --, modMask           = mod4Mask
+    , keys              = customKeys (const []) bindings
     }
+
+bindings conf@(XConfig {modMask = modm}) = [slock, chrome]
+    where slock  = ((modm .|. shiftMask, xK_l), spawn "slock")
+          chrome = ((modm, xK_c), spawn "google-chrome-stable")
