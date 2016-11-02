1b6835eb878a50ddbdde351bf9e1019db25918be
Stephen Demos <stphndemos@gmail.com>
2016-11-02 00:15:44 -0400

    added symlink for neovim

--- resymlink.sh
+++ resymlink.sh
@@ -1,6 +1,7 @@
 echo "Linking Files"
 ln -sfT ~/.genconfig/vim ~/.vim
 ln -sfT ~/.genconfig/xmonad ~/.xmonad
+ln -sfT ~/.genconfig/vim ~/.config/nvim
 ln -f ~/.genconfig/vimrc ~/.vimrc
 ln -f ~/.genconfig/zshrc ~/.zshrc
 ln -f ~/.genconfig/xinitrc ~/.xinitrc
