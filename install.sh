47cb1c72ae8c5e871bb386bd0f41d81ecf0e48ad
Stephen Demos <stphndemos@gmail.com>
2016-11-02 00:14:05 -0400

    drastically simplified install script

--- install.sh
+++ install.sh
@@ -1,37 +1,5 @@
 echo "Cloning dotfiles"
-hash git >/dev/null || {
-  echo "git not installed"
-  exit
-}
-hash /usr/bin/env git clone git@github.com:stphndemos/dotfiles.git ~/.genconfig || /usr/bin/env git clone https://github.com/stphndemos/dotfiles.git ~/.genconfig {
-  echo "something went wrong - the error is probably above ^^"
-  exit
-}
-
-ohmyzshyes=true;
-
-if [ "$1" ]
-then
-  if [ "$1" == "--no-oh-my-zsh" ]
-  then
-    ohmyzshyes=false;
-  fi
-  if [ "$1" == "-h" || "$1" == "--help"]
-  then
-    echo "usage: ./install.sh [--no-oh-my-zsh] [-h] [--help]"
-    exit
-  fi
-fi
-
-if [ $ohmyzshyes ]
-then
-  hash curl >/dev/null && /usr/bin/env curl -L https://github.com/stphndemos/oh-my-zsh/raw/master/tools/install.sh | sh || {
-    hash wget >/dev/null && /usr/bin/env wget --no-check-certificate https://github.com/stphndemos/oh-my-zsh/raw/master/tools/install.sh -O - | sh || {
-      echo "needs curl or wget to continue (how did you get here?)"
-      echo "you can use --no-oh-my-zsh to skip this part and install it manually"
-      exit
-    }
-  }
-fi
+git clone git@github.com:stphndemos/dotfiles.git ~/.genconfig
+curl -L https://github.com/stphndemos/oh-my-zsh/raw/master/tools/install.sh | sh
 
 ~/.genconfig/resymlink.sh
