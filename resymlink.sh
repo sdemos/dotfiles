00a44433ab086ce8b090db62f4eb2ae45a6a43a2
Stephen Demos <stphndemos@gmail.com>
2016-11-02 00:06:36 -0400

    fixed up some long standing bugs for resymlinking

--- resymlink.sh
+++ resymlink.sh
@@ -1,13 +1,11 @@
-
 echo "Linking Files"
-ln -sf ~/.genconfig/vim ~/.vim
-ln -sf ~/.genconfig/xmonad ~/.xmonad
+ln -sfT ~/.genconfig/vim ~/.vim
+ln -sfT ~/.genconfig/xmonad ~/.xmonad
 ln -f ~/.genconfig/vimrc ~/.vimrc
 ln -f ~/.genconfig/zshrc ~/.zshrc
 ln -f ~/.genconfig/xinitrc ~/.xinitrc
 ln -f ~/.genconfig/Xdefaults ~/.Xdefaults
 ln -f ~/.genconfig/gitignore_global ~/.gitignore_global
-hash git >/dev/null && /usr/bin/env git config --global core.excludesfile ~/.gitignore_global || {
-  echo "git not installed (how did you get here?)"
-  exit
-}
+
+echo "Reconfiguring"
+git config --global core.excludesfile ~/.gitignore_global
