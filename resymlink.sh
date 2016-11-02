fe285a153a719cf35be614a0f57b5f3bac8bf889
Stephen Demos <stphndemos@gmail.com>
2016-11-02 00:03:13 -0400

    updated resymlinking for new naming scheme

--- resymlink.sh
+++ resymlink.sh
@@ -1,10 +1,12 @@
 
 echo "Linking Files"
-ln -sf ~/.genconfig/.vim_backup ~/.vim
-ln -sf ~/.genconfig/.xmonad_git ~/.xmonad
-ln -f ~/.genconfig/.*rc* ~/
-ln -f ~/.genconfig/.Xdefaults ~/
-ln -f ~/.genconfig/.gitignore_global ~/
+ln -sf ~/.genconfig/vim ~/.vim
+ln -sf ~/.genconfig/xmonad ~/.xmonad
+ln -f ~/.genconfig/vimrc ~/.vimrc
+ln -f ~/.genconfig/zshrc ~/.zshrc
+ln -f ~/.genconfig/xinitrc ~/.xinitrc
+ln -f ~/.genconfig/Xdefaults ~/.Xdefaults
+ln -f ~/.genconfig/gitignore_global ~/.gitignore_global
 hash git >/dev/null && /usr/bin/env git config --global core.excludesfile ~/.gitignore_global || {
   echo "git not installed (how did you get here?)"
   exit
